1fdbf85d742a1a678fbf6e14524d78e78b9448c8
Alii_ <33094586+Popeyee@users.noreply.github.com>
2020-06-16 13:13:25 -0400

    Updates

---
+++ precache-manifest.6f7b1d9de072436aa1f8338d731cc7aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18c1ea85f2506808d5d2080db5f1f86",
+    "url": "/faceReco/index.html"
+  },
+  {
+    "revision": "2f875694b0b4d5f89bfb",
+    "url": "/faceReco/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "132bc7e2dfca47e0e8d0",
+    "url": "/faceReco/static/css/main.c53873ea.chunk.css"
+  },
+  {
+    "revision": "2f875694b0b4d5f89bfb",
+    "url": "/faceReco/static/js/2.8a1548e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceReco/static/js/2.8a1548e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132bc7e2dfca47e0e8d0",
+    "url": "/faceReco/static/js/main.b566f0a4.chunk.js"
+  },
+  {
+    "revision": "4283a9a3c7dacdc22864",
+    "url": "/faceReco/static/js/runtime-main.63d234bc.js"
+  }
+]);
