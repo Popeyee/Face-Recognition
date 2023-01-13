9cebda157358e978c8951ce08f27323456a3da00
Alii_ <33094586+Popeyee@users.noreply.github.com>
2023-01-13 17:07:47 -0500

    small fix//.,

--- src/App.js
+++ src/App.js
@@ -54,21 +54,35 @@ class App extends Component {
     }})
   }
 
+  // calculateFaceLocation = (data) => {
+  //   console.log(data);
+  //   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  //   const image = document.getElementById('inputimage');
+  //   const width = Number(image.width);
+  //   const height = Number(image.height);
+  //   console.log('w and h',width,height);
+    
+  //   return {
+  //     leftCol: clarifaiFace.left_col * width,
+  //     topRow: clarifaiFace.top_row * height,
+  //     rightCol: width - (clarifaiFace.right_col * width),
+  //     bottomRow: height - (clarifaiFace.bottom_row * height)
+  //   }
+  // }
   calculateFaceLocation = (data) => {
-    console.log(data);
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
+    const clarifaiFace = JSON.parse(data, null, 2).outputs[0].data.regions[0]
+      .region_info.bounding_box;
+      console.log(clarifaiFace)
+    const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log('w and h',width,height);
-    
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   displayFaceBox = (box) => {
     this.setState({box: box});
